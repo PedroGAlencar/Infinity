c637bfb953815014bdd79582f92a468d031c72bf
Pedro Gomes Alencar <156344012+PedroGAlencar@users.noreply.github.com>
2024-04-09 16:45:09 -0300

    Add files via upload

---
+++ prova13.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+def converter_centi_pra_metros():
+    valor = entry_centimetros.get()
+    if valor.isdigit():
+        centi = float(valor)
+        metros = centi / 100
+        metros = float(metros)
+        label_metros.config(text=f'Conversão: {metros  } metros')
+    return 'Valor inválido. Por favor, insira um número'
+
+
+
+
+
+root = Tk()
+root.title('Conversor de centímetros para metros')
+root.geometry('400x250')
+#Pegar a unidade de medida em centímetros
+label = Label(root,text='Digite uma distância em centímetros: ',font=('Arial',10))
+label.grid(row=0,column=0)
+#Espaço para o usuário digitar a distância em centímetros
+entry_centimetros = Entry(root)
+entry_centimetros.grid(row=0,column=1)
+
+#Resultado da conversão
+label_metros = Label(root,text='',font=('Arial',10,'bold'))
+label_metros.grid(row=3,column=0,columnspan=2)
+
+#Botão que converte os dados do entry para metros
+button = Button(root,text='Converter',command=converter_centi_pra_metros)
+button.grid(row=1,column=0,columnspan=2)
+
+root.mainloop()
